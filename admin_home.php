5b126103464e7e266faf5cb64244f8672146295c
reaganwanjala <147992907+reaganwanjala@users.noreply.github.com>
2023-10-27 15:51:41 +0300

    Add files via upload

---
+++ admin_home.php
@@ -0,0 +1,73 @@
+<?php
+    include "validate_admin.php";
+    include "header.php";
+    include "user_navbar.php";
+    include "admin_sidebar.php";
+    include "session_timeout.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin_home_style.css">
+</head>
+
+<body>
+    <div class="flex-container">
+        <div class="flex-item">
+            <h1 id="customer">
+                Welcome Admin !
+            </h1>
+            <p id="customer" style="max-width:800px">
+                From here you can manage all of core Internet Banking settings.
+                You can add/manage customers, view their transactions, edit their
+                details and even delete them. You can also post news on the website.
+                <br>Please keep in mind that with big power comes big responsibility.
+                Therefore please do not misuse your admin control to create trouble.
+            </p>
+        </div>
+    </div>
+
+<!-- Code injected by live-server -->
+<script>
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function () {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					var parent = elem.parentElement || head;
+					parent.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					parent.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function (msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			if (sessionStorage && !sessionStorage.getItem('IsThisFirstTime_Log_From_LiveServer')) {
+				console.log('Live reload enabled.');
+				sessionStorage.setItem('IsThisFirstTime_Log_From_LiveServer', true);
+			}
+		})();
+	}
+	else {
+		console.error('Upgrade your browser. This Browser is NOT supported WebSocket for Live-Reloading.');
+	}
+	// ]]>
+</script>
+</body>
+</html>
+
+<?php include "easter_egg.php"; ?>
